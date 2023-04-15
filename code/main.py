b357aae7e5f601a1701ebba12e747507e14fc7f8
zeazy00 <65069197+zeazy00@users.noreply.github.com>
2023-04-15 19:21:10 +1000

    format main.py

--- code/main.py
+++ code/main.py
@@ -9,41 +9,61 @@ from sj import get_sj_statistics
 
 
 def main():
-	load_dotenv()
-	token = getenv("SUPERJOB_TOKEN")
-	languages = [
-		"Python",
-		"C",
-		"Java",
-		"C++",
-		"C#",
-		"Visual Basic",
-		"JavaScript",
-		"SQL",
-		"Assembly language",
-		"Swift"
-	]
-	hh_table_statistics = [["language", "vacancies found", "vacancies processed", "average salary"]]
-	sj_table_statistics = [["language", "vacancies found", "vacancies processed", "average salary"]]
+    load_dotenv()
+    token = getenv("SUPERJOB_TOKEN")
+    languages = [
+        "Python",
+        "C",
+        "Java",
+        "C++",
+        "C#",
+        "Visual Basic",
+        "JavaScript",
+        "SQL",
+        "Assembly language",
+        "Swift"
+    ]
+    hh_table_statistics = [[
+        "language",
+        "vacancies found",
+        "vacancies processed",
+        "average salary"
+    ]]
+    sj_table_statistics = [[
+        "language",
+        "vacancies found",
+        "vacancies processed",
+        "average salary"
+    ]]
 
-	for language in languages:
-		hh_statistics = get_hh_statistics(language)
-		formated_statistics =  format_statistics(*hh_statistics)
-		line_table = [language, formated_statistics["vacancies_found"], formated_statistics["vacancies_processed"], formated_statistics["average_salary"]]
-		hh_table_statistics.append(line_table)
+    for language in languages:
+        hh_statistics = get_hh_statistics(language)
+        formated_statistics =  format_statistics(*hh_statistics)
+        line_table = [
+            language, 
+            formated_statistics["vacancies_found"],
+            formated_statistics["vacancies_processed"],
+            formated_statistics["average_salary"]
+        ]
+        hh_table_statistics.append(line_table)
 
-		sj_statistics = get_sj_statistics(language, token)
-		formated_statistics =  format_statistics(*sj_statistics)
-		line_table = [language, formated_statistics["vacancies_found"], formated_statistics["vacancies_processed"], formated_statistics["average_salary"]]
-		sj_table_statistics.append(line_table)
+        sj_statistics = get_sj_statistics(language, token)
+        formated_statistics =  format_statistics(*sj_statistics)
+        line_table = [
+            language,
+            formated_statistics["vacancies_found"],
+            formated_statistics["vacancies_processed"],
+            formated_statistics["average_salary"]
+        ]
+        sj_table_statistics.append(line_table)
 
-		system("cls")
-		table_instance = SingleTable(hh_table_statistics, "HeahHunter vacancies")
-		print(table_instance.table)
-		print()
-		table_instance = SingleTable(sj_table_statistics, "SuperJob vacancies")
-		print(table_instance.table)
+        system("cls")
+        table_instance = SingleTable(hh_table_statistics, "HeahHunter vacancies")
+        print(table_instance.table)
+        print()
+        table_instance = SingleTable(sj_table_statistics, "SuperJob vacancies")
+        print(table_instance.table)
 
 
 if __name__ == '__main__':
-	main()
+    main()
